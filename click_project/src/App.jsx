b559a89e2cb089e229bc2fcdd4a2de5d1298cf31
sheikh-mamun <abalmamun2515@gmail.com>
2024-06-10 18:33:18 +0600

    new

--- click_project/src/App.jsx
+++ click_project/src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import Navbar from './Navbar';
+import Header from './Header';
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>sheikh + mamun</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click this sheikh mamun
-      </p>
-    </>
-  )
-}
+    <div>
+      <Navbar/>
+      <Header/>
 
-export default App
+      
+    </div>
+  );
+};
+
+export default App;
