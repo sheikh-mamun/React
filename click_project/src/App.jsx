5d484ac6de3b530fc6e8b911a12829d425b240bf
sheikh-mamun <abalmamun2515@gmail.com>
2024-06-12 11:50:02 +0600

    new page and components

--- click_project/src/App.jsx
+++ click_project/src/App.jsx
@@ -1,18 +1,24 @@
-
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
 
 const App = () => {
-  return (
-    <div>
-      <Navbar/>
-      <Header/>
-      <Footer/>
-
-      
-    </div>
-  );
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/services" element={<Services />} />
+                <Route path="/contact" element={<Contact />} />
+            </Routes>
+        </Router>
+    );
 };
 
 export default App;
---
+++ click_project/src/pages/Service.jsx
@@ -0,0 +1,8 @@
+// src/Services.js
+import React from 'react';
+
+const Services = () => {
+    return <h1>Services Page</h1>;
+};
+
+export default Services;
